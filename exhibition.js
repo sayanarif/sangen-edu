d71ead6860291fdf5daafe2b293390dc34f5fd10
Md Ariful Islam <arif22113@gmail.com>
2021-02-19 01:22:03 +0600

    Last Update

---
+++ exhibition.js
@@ -0,0 +1,62 @@
+<section class="area-padding">
+    <div class="container">
+        <div class="section-title">
+            <h1>Mission & Vision of Sangen Edu Ltd.</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="section-title">
+                    <h2><span>What We Want To Do</span> Our Mission</h2>
+                </div>
+                <p>Likewise other reputed organizations, we have some mottos to
+                    fulfill and goals to achieve.</p>
+                <ul class="ul-style-dark">
+                    <li>To encourage and support the students to get overseas’ degree in China, the
+                        The UK, the USA, Canada, Australia, India, and Europe.</li>
+                    <li>To provide up-to-date information and course details of the universities and the necessary information about study abroad through a free counseling service.</li>
+                    <li>To help the students finding a quick and easy but a
+                        more cost-effective way for study in the universities of the above-mentioned countries.</li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <div class="svc-service-area">
+                    <div class="image-area">
+                        <img src="/images/featured/team-sangen.jpg" alt="Our Mission in Sangen Edu Ltd."></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="svc-service-area">
+                    <div class="image-area">
+                        <img src="/images/featured/team-sangen.jpg" alt="Our Mission in Sangen Edu Ltd."></div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <ul class="ul-style-dark">
+                    <li>To give a platform to the students for getting a world-class education and become successful in the competitive world.</li>
+                    <li>To provide the information about the scholarship program in specific universities in China and other countries.</li>
+                    <li>To provide the services for admission and after admission. Our care and concern continue from start to end. We offer first-hand assistance to students studying in China’s institutions, including airport pickup services, arranging accommodations, and even opening bank accounts. We stay in regular contact with our students and liaise with universities as well as parents in monitoring students’ academic progress.</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="section-title">
+                    <h2><span>What We Want To Do</span> Our Vision</h2>
+                </div>
+                <ul class="ul-style-dark">
+                    <li>Our paramount vision is to nurture and serve our students to achieve their goals and fulfill their dreams. We want to provide our students with the best education service possible, and a wanted and meaningful life. </li>
+                    <li>Their every success in life is our sole accomplishment. We want to provide proper guidance to the students, to choose a better career and to reach the zenith of achievement.</li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <div class="svc-service-area">
+                    <div class="image-area">
+                        <img src="/images/featured/team-sangen.jpg" alt="Our Vission in Sangen Edu Ltd."></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
